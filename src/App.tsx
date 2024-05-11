c0b9fcc5c5a7ca61bbe64e8474d1f2422ebdbb56
Alex Glancevs <alex2531458@gmail.com>
2024-05-11 21:07:03 +0300

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles/App.scss';
+
+class App extends React.Component {
+  constructor(props: any) {
+    super(props);
+    this.state = {};
+  }
+
+  render(): React.ReactNode {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default App;
