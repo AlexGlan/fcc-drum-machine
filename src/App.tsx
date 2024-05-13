945412ee1d59a1308030a90ff546c11a71c3748e
Alex Glancevs <alex2531458@gmail.com>
2024-05-13 23:43:11 +0300

    refactor: change App from class to functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,33 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFreeCodeCamp } from '@fortawesome/free-brands-svg-icons';
 import DrumPad from './components/DrumPad';
 import data from './data/pad-mappings.json';
 import './styles/App.scss';
 
-type AppState = {
-  bankPosition: string
-}
+const App = () => {
+    const [bankPosition, setBankPosition] = useState('left');
+    const [drumPadItems, setDrumPadItems] = useState<JSX.Element[]>([]);
 
-class App extends React.Component<{}, AppState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      bankPosition: 'left'
-    };
-  }
-
-  render(): React.ReactNode {
-    const drumPadItems = data
-      .filter(pad => pad.bankPosition === this.state.bankPosition)
-      .map(pad => ((
-        <DrumPad key={pad.id} {...pad} ></DrumPad>
-      )));
+    useEffect(() => {
+        const drumPadItems: JSX.Element[] = data
+            .filter(pad => pad.bankPosition === bankPosition)
+            .map(pad => <DrumPad key={pad.id} {...pad} />);
+        
+        setDrumPadItems(drumPadItems);
+    }, [bankPosition])
     
     return (
-      <main id="drum-machine">
-        <div className="top-bar">
-          <span className='top-bar__text'>FCC</span>
-          <FontAwesomeIcon icon={faFreeCodeCamp} className='top-bar__logo' />
-        </div>
+        <main id="drum-machine">
+            <div className="top-bar">
+                <span className='top-bar__text'>FCC</span>
+                <FontAwesomeIcon icon={faFreeCodeCamp} className='top-bar__logo' />
+            </div>
 
-        <div className='drum-pads'>{drumPadItems}</div>
+            <div className='drum-pads'>{drumPadItems}</div>
 
-      </main>
+        </main>
     );
-  }
 }
 
 export default App;
