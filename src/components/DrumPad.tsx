9dac73d093ac8a6b035022d6bc3c1381fa712b88
Alex Glancevs <alex2531458@gmail.com>
2024-05-15 20:47:34 +0300

    feature: add the ability to play audio from DrumPad component

--- src/components/DrumPad.tsx
+++ src/components/DrumPad.tsx
@@ -1,17 +1,44 @@
+import { useEffect, useRef } from 'react';
 import '../styles/DrumPad.scss';
 
 type DrumPadProps = {
-    id: number,
     keyCap: string,
     name: string,
-    bankPosition: string,
     audio: string
 }
 
-const DrumPad = ({ id, keyCap, name, bankPosition, audio }: DrumPadProps) => {
+const DrumPad = ({ keyCap, name, audio }: DrumPadProps): JSX.Element => {
+    const audioRef = useRef<HTMLAudioElement>(null);
+
+    const playAudio = (): void => {
+        if (audioRef.current) {
+            audioRef.current.currentTime = 0;
+            audioRef.current.play();
+        }        
+    }
+
+    const handleKeyDown = (e: KeyboardEvent): void => {
+        if (e.key === keyCap.toLowerCase()) {
+            playAudio();  
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('keydown', handleKeyDown);
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        }
+    }, [])
+        
     return (
-        <div className="drum-pad" id={name}>
+        <div onClick={ () => {playAudio()} } className="drum-pad" id={name}>
             <span>{keyCap}</span>
+            <audio
+                ref={audioRef}
+                src={audio}
+                className='clip'
+                id={keyCap}
+            ></audio>
         </div>
     )
 }
